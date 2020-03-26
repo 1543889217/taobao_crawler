3af3a271fb6e292832be36645b9037e2e4da1153
junges521 <junges521@163.com>
2020-03-26 17:22:40 +0800

    Update test.bat

--- test.bat
+++ test.bat
@@ -1,4 +1,4 @@
-adb shell am start -n com.jxl.talentscout.version1/com.xxx.talentscout.douyin.service.ServiceActivity
+adb shell am start -n com.bbb.talentscout.version1/com.xxx.talentscout.douyin.service.ServiceActivity
 adb shell am start -n com.xingin.xhs/.XhsLoginActivity
 adb shell am start -n com.taobao.idlefish/com.xxxx.fuckxianyu.MainActivity
 adb shell am start -n com.fake.taobao/com.fake.taobao.ServiceActivity
